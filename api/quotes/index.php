0087db9cbefa8e143578bd5ba8e1ac314ad23da1
sisemorea <addysonsisemore@gmail.com>
2023-03-21 00:13:33 -0500

    Updated headers

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,15 +1,17 @@
 <?php
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: POST, PUT, DELETE, GET');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
 
-
-    $request_method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+}
     $data = json_decode(file_get_contents('php://input'));
 
-    switch ($request_method){
+    switch ($method){
         case 'GET':
             if (isset($_GET['id'])) {
                 require 'read_single.php';
